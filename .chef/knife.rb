6e4c568b0025a69bd860ad70107632ef85a8415c
fr-itaya <itaya@fact-real.com>
2014-10-19 07:47:12 +0900

    #3 prepared knife-solo

---
+++ .chef/knife.rb
@@ -0,0 +1,8 @@
+log_level                :info
+log_location             STDOUT
+node_name                'YItaya'
+client_key               'C:/mingw64/msys/home/YItaya/.chef/YItaya.pem'
+validation_client_name   'chef-validator'
+validation_key           'C:/etc/chef-server/chef-validator.pem'
+chef_server_url          'https://YItaya-PC:443'
+syntax_check_cache_path  'C:/mingw64/msys/home/YItaya/.chef/syntax_check_cache'
